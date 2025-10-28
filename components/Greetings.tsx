5286ac1c3b2b5e9db5897789ee23a4dd7975cd64
christian celentano <32181044+cribus@users.noreply.github.com>
2025-10-28 11:10:09 +0100

    Update Greetings.tsx

--- components/Greetings.tsx
+++ components/Greetings.tsx
@@ -86,7 +86,7 @@ const Greetings = () => {
           <li>
             Telegram: <a href="https://t.me/cribus">@cribus_tech</a>
           </li>
-            <li>
+          <li>
             Telegram Canale Offere : <a href="https://amzn.to/3Vzdkxv">@Offerte_tech</a>
           </li>
           <li>Location: Campania, Italy (remote services available)</li>
